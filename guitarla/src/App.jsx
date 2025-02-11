a05701ffae3908ec57f0e9b7f98b04005d3190f1
ArevaloDev <arevalodaniel999@gmail.com>
2025-02-11 17:39:38 -0600

    evitando que se agregue el mismo item al carrito, y que se actualice la cantidad del producto

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,21 +1,20 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Guitar from './components/Guitar'
-import Header from './components/Header'
-import { db } from './data/db'
+import { useState } from "react";
+import "./App.css";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 function App() {
-
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
-  function addToCart(item){
-    let itemExist = cart.findIndex(guitar => guitar.id === item.id);
+  function addToCart(item) {
+    let itemExist = cart.findIndex((guitar) => guitar.id === item.id);
     console.log(itemExist);
-    if(itemExist >= 0){
+    if (itemExist >= 0) {
       let updateCart = [...cart];
       updateCart[itemExist].quantity++;
       setCart(updateCart);
-    }else{
+    } else {
       item.quantity = 1;
       setCart([...cart, item]);
     }
@@ -23,30 +22,25 @@ function App() {
 
   return (
     <>
-        <Header/>
-    <main className="container-xl mt-5">
+      <Header />
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar 
-            key={guitar.id} 
-            guitar={guitar} 
-            setCart={setCart}
-            addToCart={addToCart}
-            />
-
-           ))}
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
         </div>
-    </main>
+      </main>
 
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
-
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- guitarla/src/components/Guitar.jsx
+++ guitarla/src/components/Guitar.jsx
@@ -1,4 +1,4 @@
-export default function Guitar({ guitar, setCart, addToCart}) {
+export default function Guitar({ guitar, addToCart}) {
   // console.log(guitar);
   const { id, name, image, description, price } = guitar;
 
@@ -15,7 +15,7 @@ export default function Guitar({ guitar, setCart, addToCart}) {
       </div>
       <div className="col-8">
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
-        <p>{guitar.description}</p>
+        <p>{description}</p>
         <p className="fw-black text-primary fs-3">{price}</p>
         <button
           type="button"
